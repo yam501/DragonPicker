b174db1b47fe414c6cdb6774ee4687ec3af75a9d
yam501 <ya.misha501@gmail.com>
2023-10-08 20:27:25 +0500

    eighth video

--- My project/Assets/Scenes/DragonPicker.cs
+++ My project/Assets/Scenes/DragonPicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DragonPicker : MonoBehaviour
 {
@@ -12,13 +13,18 @@ public class DragonPicker : MonoBehaviour
 
     public float energyShieldRadius = 1.5f;
 
+    public List<GameObject> shieldList;
+
     void Start()
     {
+        shieldList = new List<GameObject>();
+
         for (int i = 1; i <= numEnergyShield; i++)
         {
             GameObject tShieldGo = Instantiate<GameObject>(energyShieldPrefab);
             tShieldGo.transform.position = new Vector3(0, energyShieldBottomY, 0);
             tShieldGo.transform.localScale = new Vector3(1 * i, 1 * i, 1 * i);
+            shieldList.Add(tShieldGo);  
         }
     }
 
@@ -33,6 +39,15 @@ public class DragonPicker : MonoBehaviour
         foreach (GameObject tGO in tDragonEggArray)
         {
             Destroy(tGO);
+        }
+        int shieldIndex = shieldList.Count - 1; 
+        GameObject tShieldGo = shieldList[shieldIndex];
+        shieldList.RemoveAt(shieldIndex);
+        Destroy(tShieldGo);
+
+        if (shieldList.Count == 0)
+        {
+            SceneManager.LoadScene("_0Scene");
         }
     }
 }
